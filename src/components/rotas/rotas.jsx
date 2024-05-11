ece225c65edb002f600392466f695413fd19685b
Arthur Macêdo <121770994+Arthur-Macedo@users.noreply.github.com>
2024-05-11 20:51:43 -0300

    Update rotas.jsx

--- src/components/rotas/rotas.jsx
+++ src/components/rotas/rotas.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Login from '../login/Login';
 import Registro from '../registro/Registro';
 
 const RoutesApp = () => {
     return (
-        <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
       </Routes>
-    </BrowserRouter>
+     </HashRouter>
     );
 };
 
